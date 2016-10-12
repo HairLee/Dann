e66af9af86699d58c994c1d4c01ab2db751ed1d5
ambition9xx <hailpt.hut@gmail.com>
2016-10-12 22:34:37 +0700

    cm

--- app/src/main/java/com/hailpt/com/youtubeeng/MainActivity.java
+++ app/src/main/java/com/hailpt/com/youtubeeng/MainActivity.java
@@ -9,14 +9,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.widget.ListView;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.VolleyLog;
 import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.hailpt.com.youtubeeng.adater.CustomListAdapter;
 import com.hailpt.com.youtubeeng.app.AppController;
 import com.hailpt.com.youtubeeng.model.Movie;
+import com.hailpt.com.youtubeeng.util.CustomRequest;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -33,8 +39,8 @@ public class MainActivity extends Activity {
     private static final String TAG = MainActivity.class.getSimpleName();
 
     // Movies json url
-    private static final String url = "http://api.androidhive.info/json/movies.json";
-//    private  static final String url ="https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=PLb6mJfxRt42z-Y45aaZ5y02Wn7L7-vJ_r&maxResults=50&key=AIzaSyCFCWKR5iGgfZtRS8w8qzEfmruKa616uaI";
+//    private static final String url = "http://api.androidhive.info/json/movies.json";
+    private  static final String url ="https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=PLb6mJfxRt42z-Y45aaZ5y02Wn7L7-vJ_r&maxResults=50&key=AIzaSyCFCWKR5iGgfZtRS8w8qzEfmruKa616uaI";
     private ProgressDialog pDialog;
     private List<Movie> movieList = new ArrayList<Movie>();
     private ListView listView;
@@ -44,7 +50,42 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        getData();
+        makeJsonObjectRequest();
+    }
 
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        hidePDialog();
+    }
+
+    private void hidePDialog() {
+        if (pDialog != null) {
+            pDialog.dismiss();
+            pDialog = null;
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    public void getData2(){
+
+//        RequestQueue requestQueue = Volley.newRequestQueue(this);
+//        CustomRequest jsObjRequest = new CustomRequest(Request.Method.POST, url, params, this.createRequestSuccessListener(), this.createRequestErrorListener());
+//
+//        requestQueue.add(jsObjRequest);
+
+
+    }
+
+
+    public void getData(){
         listView = (ListView) findViewById(R.id.list);
         adapter = new CustomListAdapter(this, movieList);
         listView.setAdapter(adapter);
@@ -71,9 +112,9 @@ public class MainActivity extends Activity {
                             try {
                                 JSONObject obj = response.getJSONObject(i);
                                 Movie movie = new Movie();
-                                movie.setTitle(obj.getString("kind"));
+                                movie.setTitle(obj.getString("title"));
                                 movie.setThumbnailUrl(obj.getString("image"));
-                                movie.setRating(((Number) obj.get("etag"))
+                                movie.setRating(((Number) obj.get("rating"))
                                         .doubleValue());
                                 movie.setYear(obj.getInt("releaseYear"));
 
@@ -111,23 +152,72 @@ public class MainActivity extends Activity {
         AppController.getInstance().addToRequestQueue(movieReq);
     }
 
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        hidePDialog();
-    }
+    /**
+     * Method to make json object request where json response starts wtih {
+     * */
+    private String jsonResponse;
+    private void makeJsonObjectRequest() {
 
-    private void hidePDialog() {
-        if (pDialog != null) {
-            pDialog.dismiss();
-            pDialog = null;
-        }
-    }
+        pDialog = new ProgressDialog(this);
+        // Showing progress dialog before making http request
+        pDialog.setMessage("Loading...");
+        pDialog.show();
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+        JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.GET,
+                url, null, new Response.Listener<JSONObject>() {
+
+            @Override
+            public void onResponse(JSONObject response) {
+                Log.d(TAG, response.toString());
+
+                try {
+                    // Parsing json object response
+                    // response will be a json object
+                    String name = response.getString("kind");
+                    String email = response.getString("etag");
+//                    String nextPageToken = response.getString("nextPageToken");
+
+                    JSONObject phone = response.getJSONObject("pageInfo");
+                    int totalResults = phone.getInt("totalResults");
+                    int resultsPerPage = phone.getInt("resultsPerPage");
+
+                    JSONArray items = response.getJSONArray("items");
+
+                    for(int i =0;i<items.length();i++){
+                        JSONObject item = items.getJSONObject(i);
+                        JSONObject snippet = item.getJSONObject("snippet");
+                        Log.d("hailpt ======","Item = "+snippet.getString("title"));
+                    }
+
+                    jsonResponse = totalResults+" === "+ resultsPerPage;
+                    jsonResponse += "Name: " + name + "\n\n";
+                    jsonResponse += "Email: " + email + "\n\n";
+
+                    Toast.makeText(getApplicationContext(),
+                            jsonResponse,
+                            Toast.LENGTH_LONG).show();
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    Toast.makeText(getApplicationContext(),
+                            "Error: " + e.getMessage(),
+                            Toast.LENGTH_LONG).show();
+                }
+                hidePDialog();
+            }
+        }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.d(TAG, "Error: " + error.getMessage());
+                Toast.makeText(getApplicationContext(),
+                        error.getMessage(), Toast.LENGTH_SHORT).show();
+                // hide the progress dialog
+                hidePDialog();
+            }
+        });
+
+        // Adding request to request queue
+        AppController.getInstance().addToRequestQueue(jsonObjReq);
     }
 }
